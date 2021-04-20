ebcb87e26668d9da97450652549113ff78d9efec
zbazhina <80741385+zbazhina@users.noreply.github.com>
2021-04-20 11:51:49 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+//alert("Hello World!");
+
+var colorArray = ["#5A9C6E","#A8BF5A","#CCC","#000","#00ff00"];
+
+var i = 0;
+
+function changeColor() {
+
+document.body.style.background = colorArray[i];
+
+i++; 
+
+console.log(colorArray[i]);
+
+
+if (i > colorArray.length - 1) {
+	i = 0;
+
+}
+
+}
+
+function selectColor() {
+	let color = document.getElementById("color").value;
+
+	document.body.style.background = color;
+
+	console.log(color)
+}
